e0e3f9030e10ac4b67ef1443de6789733aaa58f1
Zak Barbuto <zbarbuto@nextfaze.com>
2020-07-03 11:20:36 +0930

    Update to support angular 9 and 10

--- src/rollbar.module.ts
+++ src/rollbar.module.ts
@@ -1,34 +1,28 @@
-import { ModuleWithProviders, NgModule } from '@angular/core';
-import { RollbarService, ROLLBAR_CONFIGURATION } from './rollbar.service';
-import { Configuration} from 'rollbar';
+import { ModuleWithProviders, NgModule } from "@angular/core";
+import { Configuration } from "rollbar";
+import { RollbarService, ROLLBAR_CONFIGURATION } from "./rollbar.service";
 
 @NgModule({
-    providers: [
-        RollbarService 
-    ],
+  providers: [RollbarService],
 })
 /**
  * Rollbar Logging support for Angular 2.
  */
 export class RollbarModule {
-
-    /**
-     * 
-     * Setup the module in your application's root bootstrap.
-     * 
-     * @static
-     * @param {RollbarConfig} config The configuration for Rollbar logging
-     * @returns {ModuleWithProviders} The module ready for your bootstrap
-     * 
-     * @memberOf RollbarModule
-     */
-    static forRoot(config: Configuration): ModuleWithProviders {
-
-        return {
-            ngModule: RollbarModule,
-            providers: [
-                {provide: ROLLBAR_CONFIGURATION, useValue: config}
-            ],
-        }
-    }
+  /**
+   *
+   * Setup the module in your application's root bootstrap.
+   *
+   * @static
+   * @param {RollbarConfig} config The configuration for Rollbar logging
+   * @returns {ModuleWithProviders} The module ready for your bootstrap
+   *
+   * @memberOf RollbarModule
+   */
+  static forRoot(config: Configuration): ModuleWithProviders<RollbarModule> {
+    return {
+      ngModule: RollbarModule,
+      providers: [{ provide: ROLLBAR_CONFIGURATION, useValue: config }],
+    };
+  }
 }
